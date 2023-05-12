46b30970fbe99eed35409e6f0799d064e7711b31
Dago04 <dagoscr@gmail.com>
2023-05-12 12:33:01 -0600

    a

---
+++ Semana 7/footer.js
@@ -0,0 +1,111 @@
+var contenidoFooter = document.querySelector("#footerID");
+
+contenidoFooter.innerHTML += `<footer class=" footer text-center text-lg-start text-dark mt-3" style="background-color: gray">
+
+<section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+    <div class="me-5 d-none d-lg-block">
+        <span>Contactate conmigo</span>
+    </div>
+
+
+
+    <div>
+        <a href="" class="me-4 text-reset">
+            <i class="bi bi-github"></i> 
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="bi bi-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="bi bi-whatsapp"></i>
+
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="bi bi-envelope"></i>
+        </a>
+
+    </div>
+
+</section>
+
+
+
+<section class="">
+    <div class="container text-center text-md-start mt-5">
+
+        <div class="row mt-3">
+
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                    <i class="fas fa-gem me-3"></i>Natural Lodge
+                </h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati perspiciatis minus beatae
+                    explicabo,
+                    corporis porro quidem? Adipisci assumenda neque voluptatem incidunt doloribus, impedit
+                    repellat facilis id
+                    aliquam quisquam, maxime molestias!
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                    Sobre Nosotros
+                </h6>
+                <p>
+                    <a href="#!" class="text-reset">Angular</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">React</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Vue</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Laravel</a>
+                </p>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                    Habitaciones
+                </h6>
+                <p>
+                    <a href="#!" class="text-reset">Pricing</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Settings</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Orders</a>
+                </p>
+                <p>
+                    <a href="#!" class="text-reset">Help</a>
+                </p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">Contact0</h6>
+                <p><i class="fas fa-home me-3"></i> La Tigra, San Carlos</p>
+                <p>
+                    <i class="fas fa-envelope me-3"></i>
+                    Naturallodge@gmail.com
+                </p>
+                <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+
+
+</footer>
+`
