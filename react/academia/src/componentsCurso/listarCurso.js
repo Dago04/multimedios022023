0247882098c2b25e5b04f947602fe05035fa59b3
Dago04 <dagoscr@gmail.com>
2023-06-16 17:52:16 -0600

    react update

--- react/academia/src/componentsCurso/listarCurso.js
+++ react/academia/src/componentsCurso/listarCurso.js
@@ -3,31 +3,39 @@ import React from 'react';
 class ListarCurso extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            datosCargados : false,
-            datosCursos:[],
+        this.state = {
+            datosCargados: false,
+            datosCursos: [],
         }
     }
 
-    cargarDatos(){        
+    cargarDatos() {
         fetch("https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php")//url de peticion de datos
-        .then(respuesta => respuesta.json())//recibe los datos en formato json
-        .then((datosrepuesta) => {
-            this.setState({datosCargados:true,  datosCursos:datosrepuesta.data})
-            console.log(datosrepuesta.data);
-        })
-        .catch(console.log)//muestra errores
+            .then(respuesta => respuesta.json())//recibe los datos en formato json
+            .then((datosrepuesta) => {
+                this.setState({ datosCargados: true, datosCursos: datosrepuesta.data })
+                console.log(datosrepuesta.data);
+            })
+            .catch(console.log)//muestra errores
+    }
+    eliminar(id){
+        console.log(id);
     }
 
-    componentDidMount(){
+    editar(objeto){
+        console.log(objeto);
+    }
+
+    componentDidMount() {
         this.cargarDatos();
     }
-   
-    render() { 
-        const { datosCargados, datosCursos } =this.state
-        return ( 
+
+
+    render() {
+        const { datosCargados, datosCursos } = this.state
+        return (
             <div className='container'>
-                <h1>Listar Curso</h1>
+                <h1 className='p-3' >Listar Curso</h1>
                 <div className="table-responsive">
                     <table className="table table-primary">
                         <thead>
@@ -41,8 +49,26 @@ class ListarCurso extends React.Component {
                             </tr>
                         </thead>
                         <tbody>
-                         {
-                           
+                            {
+                                datosCursos.map(
+                                    (datosExtraidos) => (
+
+                                        <tr key={datosExtraidos.id} className="table-primary table-striped" >
+                                            <td scope="row">{datosExtraidos.id}</td>
+                                            <td>{datosExtraidos.nombre}</td>
+                                            <td>{datosExtraidos.descripcion}</td>
+                                            <td>{datosExtraidos.tiempo}</td>
+                                            <td>{datosExtraidos.usuario}</td>
+                                            <td>
+                                                <a name="" id="" className="btn btn-danger mx-2" onClick={()=>this.eliminar(datosExtraidos.id)} role="button">Eliminar</a>
+                                                ||
+                                                <a name="" id="" className="btn btn-primary mx-2" onClick={()=>this.editar(datosExtraidos)} role="button">Editar</a>
+                                                
+                                            </td>
+
+                                        </tr>
+                                )
+                            )
                          }
                         </tbody>
                     </table>
